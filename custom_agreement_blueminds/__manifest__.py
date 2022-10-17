0ecb90bfe862f20f20920edc5ae705596c6e0005
Daniel Fernandez <dfernandez@daniel.localdomain>
2022-10-17 15:06:14 -0300

    modulos faltantes

---
+++ custom_agreement_blueminds/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Reporte de contratos Klug',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'agreement_blueminds'
+    ],
+    'data': [
+        'report/agreement.xml',
+        'views/agreement_view.xml'
+    ],
+   
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
