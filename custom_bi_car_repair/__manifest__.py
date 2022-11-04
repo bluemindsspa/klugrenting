9a7c748a2f4da5001f592935728e2ae45e3caec7
Daniel Fernandez <dfernandez@daniel.localdomain>
2022-11-04 02:50:18 -0300

    modulo de servicio tecnico para klug servicios

---
+++ custom_bi_car_repair/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Herencia modulo de reparacion de autos',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'bi_car_repair_management', 'account'
+    ],
+    'data': [
+        'views/car_management_view.xml',
+        'report/car_diagnosys.xml'
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
