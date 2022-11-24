1a86393b3e047e85a95a5ffd1eb0c711121ae7ed
Daniel Fernandez <dfernandez@daniel.localdomain>
2022-11-24 09:03:17 -0300

    cambios en flota y reportes de subscripciones listo a produccion

---
+++ custom_fleet/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Cambios al modulo de flota',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'fleet', 'base', 'sale_subscription', 'purchase', 'account'
+    ],
+    'data': [
+        'security/ir.model.access.csv',        
+        'views/fleet_view.xml'
+    ],
+
+    'auto_install': False,
+    'application': False,
+    'assets': {
+
+    }
+}
